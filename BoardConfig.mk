d3e534985d8e4fc2e9db104a2c4b57db0f9a61f0
Orca Project <shamukillaz@orcaproject.co>
2013-07-15 17:09:59 -0400

    Orca 2 Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,12 +24,10 @@
 -include device/samsung/d2-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
--include vendor/samsung/d2usc/BoardConfigVendor.mk
+-include vendor/samsung/d2r530/BoardConfigVendor.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := d2usc
 
 # Kernel
-TARGET_KERNEL_CONFIG    := aokp_d2usc_defconfig
-
-
+TARGET_KERNEL_CONFIG           := cyanogen_d2-r530_defconfig
